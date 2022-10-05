48c3b62b93618c94ebc9d28c3c776abf5102dc98
JaYun <tlfvm04@naver.com>
2022-10-06 00:50:58 +0900

    init 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import  express  from "express";
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req,res) => {
-    res.send("Hello World");
-})
+app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/about', (req, res) => res.send('About Page Route'));
+app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('express server is learing : http://localhost:3000');
 })
